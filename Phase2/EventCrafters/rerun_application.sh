836c8765f3aa1825cdaaa1f4a8169fc9ef9e76db
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2024-04-01 13:39:32 +0200

    Update rerun_application.sh
    Co-Authored-By: Lucía Domínguez Rodrigo <117301166+LuciaDominguezRodrigo@users.noreply.github.com>

--- Phase2/EventCrafters/rerun_application.sh
+++ Phase2/EventCrafters/rerun_application.sh
@@ -1,10 +1,20 @@
+#Limpieza de contenedores, imagenes y volumenes
 docker compose down
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 sudo docker volume prune
 cd ~
+
+#Nuevo clonado del proyecto
 sudo rm -r webapp11
 git clone https://github.com/CodeURJC-DAW-2023-24/webapp11.git
 cd webapp11/Phase2/EventCrafters
+
+#Construccion y subida de la imagen
 docker build -t luciadominguezrodrigo/eventcrafters .
+echo "916901930Aa." | docker login -u "luciadominguezrodrigo" --password-stdin
+docker push luciadominguezrodrigo/eventcrafters:latest
+docker logout
+
+#Arranque del proyecto
 docker compose up
