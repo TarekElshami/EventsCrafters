f58c923d4ffbbec73f2d1ad80ad22a2961e0a17c
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2024-04-01 14:25:37 +0200

    Simplified create_image.sh

--- Phase2/EventCrafters/create_image.sh
+++ Phase2/EventCrafters/create_image.sh
@@ -9,12 +9,6 @@ docker compose down
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 sudo docker volume prune
-cd ~
-
-#Nuevo clonado del proyecto
-sudo rm -r webapp11
-git clone https://github.com/CodeURJC-DAW-2023-24/webapp11.git
-cd webapp11/Phase2/EventCrafters
 
 #Construccion y subida de la imagen
 docker build -t $DOCKER_REPO:$DOCKER_TAG .
