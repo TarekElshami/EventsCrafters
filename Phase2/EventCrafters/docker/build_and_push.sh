1e833ddd3934d2e6dd4c5da0ea103b5ea51433d0
Lucía Domínguez Rodrigo <l.dominguez.2021@alumnos.urjc.es>
2024-04-01 11:51:31 +0200

    Update dockerfile
    Co-Authored-By: Marcos Jiménez <117301291+MarJ03@users.noreply.github.com>

--- Phase2/EventCrafters/docker/build_and_push.sh
+++ Phase2/EventCrafters/docker/build_and_push.sh
@@ -6,8 +6,14 @@ DOCKER_PASSWORD="916901930Aa."
 DOCKER_REPO="luciadominguezrodrigo/eventcrafters"
 DOCKER_TAG="latest"
 
+# Obtener la ruta absoluta del directorio actual
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# Navegar hacia arriba para acceder a la carpeta src
+PROJECT_DIR=$(realpath "$SCRIPT_DIR/..")
+
 # Construir la imagen Docker
-docker build -t $DOCKER_REPO:$DOCKER_TAG .
+docker build -t $DOCKER_REPO:$DOCKER_TAG -f $SCRIPT_DIR/Dockerfile $PROJECT_DIR
 
 # Etiquetar la imagen
 docker tag $DOCKER_REPO:$DOCKER_TAG $DOCKER_USERNAME/$DOCKER_REPO:$DOCKER_TAG
