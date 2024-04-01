c7522d5a6761d4c28a3a98fbc3a98c9087277be0
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2024-04-01 13:48:25 +0200

    Update rerun_application.sh
    Co-Authored-By: Lucía Domínguez Rodrigo <117301166+LuciaDominguezRodrigo@users.noreply.github.com>

--- Phase2/EventCrafters/rerun_application.sh
+++ Phase2/EventCrafters/rerun_application.sh
@@ -1,3 +1,9 @@
+# Definir variables
+DOCKER_USERNAME="luciadominguezrodrigo"
+DOCKER_PASSWORD="916901930Aa."
+DOCKER_REPO="$DOCKER_USERNAME/eventcrafters"
+DOCKER_TAG="latest"
+
 #Limpieza de contenedores, imagenes y volumenes
 docker compose down
 docker rm $(docker ps -a -q)
@@ -11,9 +17,9 @@ git clone https://github.com/CodeURJC-DAW-2023-24/webapp11.git
 cd webapp11/Phase2/EventCrafters
 
 #Construccion y subida de la imagen
-docker build -t luciadominguezrodrigo/eventcrafters .
-echo "916901930Aa." | docker login -u "luciadominguezrodrigo" --password-stdin
-docker push luciadominguezrodrigo/eventcrafters:latest
+docker build -t $DOCKER_REPO:$DOCKER_TAG .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push $DOCKER_REPO:$DOCKER_TAG
 docker logout
 
 #Arranque del proyecto
