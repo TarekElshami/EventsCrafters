bb2efb840b1e95e739a8d43b9ff2cea4f9940682
Marcos Jiménez <m.jimenezp.2021@alumnos.urjc.es>
2024-04-02 02:59:05 +0200

    Update create_image.sh
    Fixed dockerfile route, which is a folder above docker folder. Before this fix, project only detects and runs only if there´s currently an image created.

--- Phase2/EventCrafters/docker/create_image.sh
+++ Phase2/EventCrafters/docker/create_image.sh
@@ -5,7 +5,7 @@ DOCKER_REPO="$DOCKER_USERNAME/eventcrafters"
 DOCKER_TAG="latest"
 
 #Build and push project image
-docker build -t $DOCKER_REPO:$DOCKER_TAG .
+docker build -t $DOCKER_REPO:$DOCKER_TAG ..
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_REPO:$DOCKER_TAG
 docker logout
