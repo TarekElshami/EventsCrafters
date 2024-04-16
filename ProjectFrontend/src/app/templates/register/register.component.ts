5dd1f9b585c6d2ce4dafb339958db726c2351aa2
Lucía Domínguez Rodrigo <l.dominguez.2021@alumnos.urjc.es>
2024-04-16 10:19:29 +0200

    Dynamically added image to register page
    The csrf token is removed. Register logic is still pending.
    
    Co-Authored-By: Ángel Marqués García <117300926+AngelMarquesGarcia@users.noreply.github.com>

--- ProjectFrontend/src/app/templates/register/register.component.ts
+++ ProjectFrontend/src/app/templates/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+const BASE_URL = "https://localhost:8443/api/users";
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['../../forms.css', './register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(private http: HttpClient){};
+
+  imgUrl = "/assets/logo.png";
+
 
 }
