90b9fa2d461333303e86c84a7d0597fc14635f57
AlvaroS3rrano <a.serranor.2021@alumnos.urjc.es>
2024-04-22 15:33:40 +0200

    Doker Image and improvements
    Co-Authored-By: Tarek Elshami <117302441+TarekElshami@users.noreply.github.com>

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -5,7 +5,9 @@ DOCKER_REPO="$DOCKER_USERNAME/eventcrafters"
 DOCKER_TAG="latest"
 
 #Build and push project image
-docker build -t $DOCKER_REPO:$DOCKER_TAG ..
+cd ..
+docker build -t $DOCKER_REPO:$DOCKER_TAG -f docker/Dockerfile .
+cd ./docker
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push $DOCKER_REPO:$DOCKER_TAG
 docker logout
