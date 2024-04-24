c1e427648bcdab914d0f0b518aeadb65e78e4ab1
AngelMarquesGarcia <117300926+AngelMarquesGarcia@users.noreply.github.com>
2024-04-24 16:59:52 +0200

    Fixed event descriptions being too long in home
    Co-Authored-By: Lucía Domínguez Rodrigo <117301166+LuciaDominguezRodrigo@users.noreply.github.com>

--- ProjectFrontend/src/app/templates/event-card/event-card.component.ts
+++ ProjectFrontend/src/app/templates/event-card/event-card.component.ts
@@ -12,13 +12,19 @@ import { CategoryService } from '../../services/category.service'
 export class EventCardComponent {
 
     category: Category = {id : -1, name : '', color : '', eventIdInCategories: []};
-  
+
     @Input()
     eventCard!: Event;
+    description:string="";
 
     constructor(private categoryService: CategoryService, private router: Router){}
 
     ngOnInit(){
+      this.description = this.eventCard.description;
+      if (this.description.length >= 100) {
+        this.description = this.description.substring(0,100)
+        this.description = this.description.concat("...")
+      }
         if (this.eventCard.id >= 0){
             this.findCategory();
         }
@@ -30,7 +36,7 @@ export class EventCardComponent {
               this.category = data;
             },
             error: () => {
-              this.router.navigate(['/error']); 
+              this.router.navigate(['/error']);
             }
           });
     }
@@ -38,5 +44,5 @@ export class EventCardComponent {
     viewDetails(id: number){
       this.router.navigate(['/event/'+id]);
     }
-  
+
 }
