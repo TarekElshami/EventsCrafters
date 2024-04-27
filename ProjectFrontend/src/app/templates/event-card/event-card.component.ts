dc44db22c3a541cc47a5f599278272dd22aa644a
Tarek Elshami <117302441+TarekElshami@users.noreply.github.com>
2024-04-27 10:48:19 +0200

    Put location icon on event card

--- ProjectFrontend/src/app/templates/event-card/event-card.component.ts
+++ ProjectFrontend/src/app/templates/event-card/event-card.component.ts
@@ -3,6 +3,7 @@ import { Event } from '../../models/event.model';
 import { Router } from '@angular/router';
 import { Category } from '../../models/category.model';
 import { CategoryService } from '../../services/category.service'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-event-card',
@@ -10,39 +11,41 @@ import { CategoryService } from '../../services/category.service'
   styleUrls: ['../../home.css']
 })
 export class EventCardComponent {
+  // Icons
+  faMapMarkerAlt = faMapMarkerAlt;
 
-    category: Category = {id : -1, name : '', color : '', eventIdInCategories: []};
+  category: Category = { id: -1, name: '', color: '', eventIdInCategories: [] };
 
-    @Input()
-    eventCard!: Event;
-    description:string="";
+  @Input()
+  eventCard!: Event;
+  description: string = "";
 
-    constructor(private categoryService: CategoryService, private router: Router){}
+  constructor(private categoryService: CategoryService, private router: Router) { }
 
-    ngOnInit(){
-      this.description = this.eventCard.description;
-      if (this.description.length >= 100) {
-        this.description = this.description.substring(0,100)
-        this.description = this.description.concat("...")
+  ngOnInit() {
+    this.description = this.eventCard.description;
+    if (this.description.length >= 100) {
+      this.description = this.description.substring(0, 100)
+      this.description = this.description.concat("...")
+    }
+    if (this.eventCard.id >= 0) {
+      this.findCategory();
+    }
+  }
+
+  findCategory() {
+    this.categoryService.getCategoryById(this.eventCard.categoryId).subscribe({
+      next: (data) => {
+        this.category = data;
+      },
+      error: () => {
+        this.router.navigate(['/error']);
       }
-        if (this.eventCard.id >= 0){
-            this.findCategory();
-        }
-    }
+    });
+  }
 
-    findCategory(){
-        this.categoryService.getCategoryById(this.eventCard.categoryId).subscribe({
-            next: (data) => {
-              this.category = data;
-            },
-            error: () => {
-              this.router.navigate(['/error']);
-            }
-          });
-    }
-
-    viewDetails(id: number){
-      this.router.navigate(['/event/'+id]);
-    }
+  viewDetails(id: number) {
+    this.router.navigate(['/event/' + id]);
+  }
 
 }
